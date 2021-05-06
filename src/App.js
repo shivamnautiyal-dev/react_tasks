57d45dedeca2f820007bf55a90e7cef6114c3485
shivi <shivamnautiyal.nfl@gmail.com>
2021-05-06 21:25:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,14 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>SHIVAM NAUTIYAL</h1>
+    </div>
+  )
+}
+
+export default Title
